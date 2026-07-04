b1bb64a085c5d691677c31c93942ea05b8500b8c
MrKricalo <makarkrichalo@yandex.ru>
2026-07-04 12:38:23 +0300

    Испр 4.1SW

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semejka-v20'; // Подняли версию для сброса старого кэша
+const CACHE_NAME = 'semejka-v21'; // Подняли версию для сброса старого кэша
 const ASSETS = [
   '.',
   'index.html',
