77f7504d7f6a7c15f16cb27459ee1b6cecc452cf
MrKricalo <makarkrichalo@yandex.ru>
2026-07-06 09:34:19 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semejka-v26'; // Подняли версию для сброса старого кэша
+const CACHE_NAME = 'semejka-v27'; // Подняли версию для сброса старого кэша
 const ASSETS = [
   '.',
   'index.html',
