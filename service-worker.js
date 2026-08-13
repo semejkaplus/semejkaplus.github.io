d2dde9787879954457e03248a06ffd13fde03288
SEMEJKA PLUS <sirenamaster2000@gmail.com>
2026-08-13 22:29:17 +0300

    Update notification title from 'Семейка' to 'SEMEYKA'

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@ self.addEventListener('push', (event) => {
   };
 
   event.waitUntil(
-    self.registration.showNotification(data.title || 'Семейка', options)
+    self.registration.showNotification(data.title || 'SEMEYKA', options)
   );
 });
 
